9f53e84f6f8363857c1b190bd9d419c34d171f64
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2026-01-20 08:27:47 +0530

    feat: nixos configuration options
    fix: use toINIWithGlobalSection
    
    fix: there is no alias for lib.generators.toINIWithGlobalSection
    
    fix: make config options optional

--- flake.nix
+++ flake.nix
@@ -23,24 +23,65 @@
         };
       });
 
-      nixosModules.default = {config, lib, pkgs, ...}:
-        let cfg = config.services.lollipop;
-        in {
-        options.services.lollipop = {
-          enable = lib.mkEnableOption "Lollipop sticky keys service";
-        };
+      nixosModules.default =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          cfg = config.services.lollipop;
+        in
+        {
+          options.services.lollipop = {
+            enable = lib.mkEnableOption "Lollipop sticky keys service";
+            timeout = lib.mkOption {
+              type = lib.types.int;
+              description = "timeout";
+              default = 500;
+            };
+            modifiers = lib.mkOption {
+              type = lib.types.str;
+              description = "modifiers";
+              default = "leftshift,leftctrl,compose,leftmeta,fn";
+            };
+            device = lib.mkOption {
+              type = lib.types.str;
+              description = "device";
+              default = "autodetect";
+            };
+            clearAllWithEscape = lib.mkOption {
+              type = lib.types.bool;
+              description = "clear all latched and locked keys by pressing escape";
+              default = true;
+            };
+          };
 
-        config = lib.mkIf cfg.enable {
-          systemd.services.lollipop = {
-            description = "Lollipop sticky keys service";
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig = {
-              ExecStart = "${self.packages.${pkgs.system}.default}/bin/lollipop";
-              Type = "exec";
+          config = lib.mkIf cfg.enable {
+
+            systemd.services.lollipop = {
+              description = "Lollipop sticky keys service";
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+
+                ExecStart = "${self.packages.${pkgs.system}.default}/bin/lollipop ${
+                  let configContents = lib.generators.toINIWithGlobalSection { } {
+                    globalSection = {
+                      timeout = cfg.timeout;
+                      modifiers = cfg.modifiers;
+                      device = cfg.device;
+                      clear_all_with_escape = cfg.clearAllWithEscape;
+                    };
+                  }; in
+                  pkgs.writeText "config.ini" configContents
+                }";
+
+                Type = "exec";
+              };
             };
           };
         };
-      };
 
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
