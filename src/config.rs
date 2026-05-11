5658e1d6eda572a94e7cfb1a3c0e27479175df03
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2026-05-11 14:07:46 +0530

    feat: move config parsing into separate module

---
+++ src/config.rs
@@ -0,0 +1,133 @@
+use crate::Error;
+use evdev::KeyCode;
+
+#[repr(u8)]
+#[derive(PartialEq, Clone, Copy)]
+enum Section {
+    Global,
+    Touchpad,
+}
+
+pub struct Config {
+    pub(crate) modifiers: Vec<KeyCode>,
+    pub(crate) timeout: u64,
+    pub(crate) keyboard_device: Option<String>,
+    pub(crate) clear_all_with_escape: bool,
+    pub(crate) touchpad: bool,
+    pub(crate) touchpad_timeout: u64,
+    pub(crate) touchpad_slop: u64,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            touchpad_slop: 50,
+            clear_all_with_escape: true,
+            modifiers: vec![
+                KeyCode::KEY_LEFTSHIFT,
+                KeyCode::KEY_LEFTMETA,
+                KeyCode::KEY_LEFTCTRL,
+                KeyCode::KEY_LEFTALT,
+            ],
+            timeout: 500,
+            keyboard_device: None,
+            touchpad: false,
+            touchpad_timeout: 200,
+        }
+    }
+}
+
+impl Config {
+    pub fn try_from_path(config_path: &str) -> Result<Config, Error> {
+        let mut config = Config::default();
+        let mut section = Section::Global;
+        let mut newline = 0;
+        let config_string =
+            std::fs::read_to_string(config_path).map_err(|io| Error::FailedReadingConfig {
+                io,
+                path: config_path.to_string(),
+            })?;
+        for line in config_string.trim().lines() {
+            let line = line.trim();
+            match line {
+                "" => {
+                    if newline == 1 {
+                        section = Section::Global;
+                        newline = 0;
+                    }
+                    newline += 1;
+                    continue;
+                }
+                "[touchpad]" => {
+                    section = Section::Touchpad;
+                    newline = 0;
+                    continue;
+                }
+                _ => {
+                    newline = 0;
+                }
+            }
+
+            let Some((key, value)) = line.split_once("=") else {
+                Err(Error::InvalidConfig(line.to_owned()))?
+            };
+
+            match (section, key, value) {
+                (Section::Global, "device", "autodetect") => {}
+                (Section::Global, "device", device_path) => {
+                    config.keyboard_device = Some(device_path.to_owned())
+                }
+                (Section::Global, "modifiers", comma_separated_modifiers) => {
+                    for modifier_str in comma_separated_modifiers.split(",") {
+                        let modifier = modifier_name_to_key_code(modifier_str)
+                            .ok_or_else(|| Error::InvalidModifier(modifier_str.to_owned()))?;
+                        config.modifiers.push(modifier);
+                    }
+                }
+                (Section::Global, "timeout", timeout_str) => match timeout_str.parse() {
+                    Ok(milliseconds) => config.timeout = milliseconds,
+                    Err(_) => Err(Error::InvalidTimeout(timeout_str.to_owned()))?,
+                },
+                (Section::Global, "clear_all_with_escape", value) => {
+                    config.clear_all_with_escape = yesnt(value, line)?
+                }
+
+                (Section::Touchpad, "timeout", timeout_str) => match timeout_str.parse() {
+                    Ok(milliseconds) => config.touchpad_timeout = milliseconds,
+                    Err(_) => Err(Error::InvalidTimeout(timeout_str.to_owned()))?,
+                },
+                (Section::Touchpad, "slop", fuzz_str) => match fuzz_str.parse() {
+                    Ok(milliseconds) => config.touchpad_slop = milliseconds,
+                    Err(_) => Err(Error::InvalidSlop(fuzz_str.to_owned()))?,
+                },
+                (Section::Touchpad, "enable", touchpad) => config.touchpad = yesnt(touchpad, line)?,
+                _ => Err(Error::InvalidConfig(line.to_owned()))?,
+            }
+        }
+        Ok(config)
+    }
+}
+
+fn yesnt(s: &str, line: &str) -> Result<bool, Error> {
+    Ok(match s.to_lowercase().as_ref() {
+        "yes" | "true" => true,
+        "no" | "false" => false,
+        _ => Err(Error::InvalidConfig(line.to_string()))?,
+    })
+}
+
+fn modifier_name_to_key_code(s: &str) -> Option<KeyCode> {
+    let ret = match s {
+        "leftshift" => KeyCode::KEY_LEFTSHIFT,
+        "rightshift" => KeyCode::KEY_RIGHTSHIFT,
+        "leftctrl" => KeyCode::KEY_LEFTCTRL,
+        "rightctrl" => KeyCode::KEY_RIGHTCTRL,
+        "compose" => KeyCode::KEY_COMPOSE,
+        "leftmeta" => KeyCode::KEY_LEFTMETA,
+        "fn" => KeyCode::KEY_FN,
+        "capslock" => KeyCode::KEY_CAPSLOCK,
+        "rightmeta" => KeyCode::KEY_RIGHTMETA,
+        _ => return None,
+    };
+    Some(ret)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use evdev::{AbsoluteAxisCode, Device, EventStream, InputEvent, KeyEvent, LedCode, LedEvent};
 use std::collections::BTreeMap;
 use std::fmt::Debug;
+use std::io;
 use std::time::{Duration, SystemTime};
-use std::{fs, io};
+mod config;
 mod touchpad;
-
 use evdev::uinput::VirtualDevice;
 use evdev::{AttributeSet, KeyCode};
 mod key_codes;
@@ -124,35 +124,6 @@ fn pick_touchpad() -> Result<Device, Error> {
         .ok_or(Error::NoKeyboardDevice)
 }
 
-pub struct Config {
-    modifiers: Vec<KeyCode>,
-    timeout: u64,
-    keyboard_device: Option<String>,
-    clear_all_with_escape: bool,
-    touchpad: bool,
-    touchpad_timeout: u64,
-    touchpad_slop: u64,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            touchpad_slop: 50,
-            clear_all_with_escape: true,
-            modifiers: vec![
-                KeyCode::KEY_LEFTSHIFT,
-                KeyCode::KEY_LEFTMETA,
-                KeyCode::KEY_LEFTCTRL,
-                KeyCode::KEY_LEFTALT,
-            ],
-            timeout: 500,
-            keyboard_device: None,
-            touchpad: false,
-            touchpad_timeout: 200,
-        }
-    }
-}
-
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("failed to open a handle to keyboard device at path {path:?}: {io}")]
@@ -203,8 +174,8 @@ async fn handle_touchpad(
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let config = match std::env::args().nth(1) {
-        Some(config_file) => parse_config(&config_file)?,
-        None => Config::default(),
+        Some(config_file) => config::Config::try_from_path(&config_file)?,
+        None => config::Config::default(),
     };
 
     let (mut keyboard, mut led_sink) = if let Some(device_path) = config.keyboard_device {
@@ -253,7 +224,6 @@ async fn main() -> Result<(), anyhow::Error> {
     loop {
         tokio::select! {
             _ = state.touchpad.timeout() => {
-                // eprintln!("timed out");
                 lollipop_virtual_device.emit(&state.release_latched())?;
                 led_sink.send_events(&[*LedEvent::new(LedCode::LED_CAPSL, state.led_state())])?;
             }
@@ -281,104 +251,4 @@ async fn main() -> Result<(), anyhow::Error> {
             }
         }
     }
-}
-
-#[repr(u8)]
-#[derive(PartialEq, Clone, Copy)]
-enum Section {
-    Global,
-    Touchpad,
-}
-
-fn parse_config(config_path: &str) -> Result<Config, Error> {
-    let mut config = Config::default();
-    let mut section = Section::Global;
-    let mut newline = 0;
-    let config_string =
-        fs::read_to_string(config_path).map_err(|io| Error::FailedReadingConfig {
-            io,
-            path: config_path.to_string(),
-        })?;
-    for line in config_string.trim().lines() {
-        let line = line.trim();
-        match line {
-            "" => {
-                if newline == 1 {
-                    section = Section::Global;
-                    newline = 0;
-                }
-                newline += 1;
-                continue;
-            }
-            "[touchpad]" => {
-                section = Section::Touchpad;
-                newline = 0;
-                continue;
-            }
-            _ => {
-                newline = 0;
-            }
-        }
-
-        let Some((key, value)) = line.split_once("=") else {
-            Err(Error::InvalidConfig(line.to_owned()))?
-        };
-
-        match (section, key, value) {
-            (Section::Global, "device", "autodetect") => {}
-            (Section::Global, "device", device_path) => {
-                config.keyboard_device = Some(device_path.to_owned())
-            }
-            (Section::Global, "modifiers", comma_separated_modifiers) => {
-                for modifier_str in comma_separated_modifiers.split(",") {
-                    let modifier = modifier_name_to_key_code(modifier_str)
-                        .ok_or_else(|| Error::InvalidModifier(modifier_str.to_owned()))?;
-                    config.modifiers.push(modifier);
-                }
-            }
-            (Section::Global, "timeout", timeout_str) => match timeout_str.parse() {
-                Ok(milliseconds) => config.timeout = milliseconds,
-                Err(_) => Err(Error::InvalidTimeout(timeout_str.to_owned()))?,
-            },
-            (Section::Global, "clear_all_with_escape", value) => {
-                config.clear_all_with_escape = yesnt(value, line)?
-            }
-
-            (Section::Touchpad, "timeout", timeout_str) => match timeout_str.parse() {
-                Ok(milliseconds) => config.touchpad_timeout = milliseconds,
-                Err(_) => Err(Error::InvalidTimeout(timeout_str.to_owned()))?,
-            },
-            (Section::Touchpad, "slop", fuzz_str) => match fuzz_str.parse() {
-                Ok(milliseconds) => config.touchpad_slop = milliseconds,
-                Err(_) => Err(Error::InvalidSlop(fuzz_str.to_owned()))?,
-            },
-            (Section::Touchpad, "enable", touchpad) => config.touchpad = yesnt(touchpad, line)?,
-            _ => Err(Error::InvalidConfig(line.to_owned()))?,
-        }
-    }
-    Ok(config)
-}
-
-fn yesnt(s: &str, line: &str) -> Result<bool, Error> {
-    Ok(match s.to_lowercase().as_ref() {
-        "yes" | "true" => true,
-        "no" | "false" => false,
-        _ => Err(Error::InvalidConfig(line.to_string()))?,
-    })
-}
-
-fn modifier_name_to_key_code(s: &str) -> Option<KeyCode> {
-    let ret = match s {
-        "leftshift" => KeyCode::KEY_LEFTSHIFT,
-        "rightshift" => KeyCode::KEY_RIGHTSHIFT,
-        "leftctrl" => KeyCode::KEY_LEFTCTRL,
-        "rightctrl" => KeyCode::KEY_RIGHTCTRL,
-        "compose" => KeyCode::KEY_COMPOSE,
-        "leftmeta" => KeyCode::KEY_LEFTMETA,
-        "fn" => KeyCode::KEY_FN,
-        "capslock" => KeyCode::KEY_CAPSLOCK,
-        "rightmeta" => KeyCode::KEY_RIGHTMETA,
-        _ => return None,
-    };
-    Some(ret)
 }
