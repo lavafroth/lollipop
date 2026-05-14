526321dfa01fb8a7f3731435b276c9415a8d98b5
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2026-05-14 08:53:25 +0530

    fix: use the consistent dbus interface name throughout the project

--- lollipop-dbus/src/main.rs
+++ lollipop-dbus/src/main.rs
@@ -1,4 +1,5 @@
-use notify::{EventKind, RecursiveMode, Result, Watcher};
+use anyhow::Result;
+use notify::{EventKind, RecursiveMode, Watcher};
 use std::path::Path;
 use tokio::runtime::Handle;
 use zbus::object_server::SignalEmitter;
@@ -6,7 +7,7 @@ use zbus::{connection::Builder, interface};
 
 struct WatcherSignal;
 
-#[interface(name = "org.kde.FileWatcher")]
+#[interface(name = "xyz.lavafroth.Lollipop")]
 impl WatcherSignal {
     #[zbus(signal)]
     async fn file_changed(ctx: &SignalEmitter<'_>, contents: &str) -> zbus::Result<()>;
@@ -15,22 +16,16 @@ impl WatcherSignal {
 #[tokio::main]
 async fn main() -> Result<()> {
     // session bus, not system bus
-    let conn = Builder::session()
-        .expect("builder failed")
-        .name("org.kde.FileWatcher")
-        .expect("builder failed")
-        .serve_at("/Object", WatcherSignal)
-        .expect("builder failed")
+    let conn = Builder::session()?
+        .name("xyz.lavafroth.Lollipop")?
+        .serve_at("/Object", WatcherSignal)?
         .build()
-        .await
-        .unwrap();
+        .await?;
 
-    let ctx: zbus::object_server::InterfaceRef<WatcherSignal> =
-        conn.object_server().interface("/Object").await.unwrap();
-
+    let ctx = conn.object_server().interface("/Object").await?;
     let handle = Handle::current();
 
-    let mut watcher = notify::recommended_watcher(move |res: Result<notify::Event>| {
+    let mut watcher = notify::recommended_watcher(move |res: notify::Result<notify::Event>| {
         if let Ok(event) = res {
             handle.block_on(async {
                 if let Some(path) = event.paths.first()
@@ -38,15 +33,19 @@ async fn main() -> Result<()> {
                 {
                     let message = tokio::fs::read_to_string(path)
                         .await
-                        .expect("failed to read file lmao");
-                    ctx.file_changed(&message).await.expect("lol");
+                        .expect("failed to read shared memory");
+                    ctx.file_changed(&message)
+                        .await
+                        .expect("failed to notify changes via dbus");
                 }
             })
         }
     })?;
 
-    watcher.watch(Path::new("/dev/shm/lollipop.shm"), RecursiveMode::Recursive)?;
+    watcher.watch(
+        Path::new("/dev/shm/lollipop.shm"),
+        RecursiveMode::NonRecursive,
+    )?;
 
-    std::future::pending::<()>().await; // aka wait forever
-    Ok(())
+    std::future::pending().await // aka wait forever
 }
